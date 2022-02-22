4b092241875b4a0c3d39efa01741864e4058716b
pepeperez13 <josepehern@gmail.com>
2022-02-22 15:53:37 +0100

    Masterstudies

---
+++ src/business/MasterStudiesGame.java
@@ -0,0 +1,37 @@
+package business;
+
+import business.playerTypes.Player;
+import business.typeTrials.MasterStudies;
+
+import java.util.Random;
+
+public class MasterStudiesGame {
+
+    //Metodo que dice si el jugador ha pasado los creditos requeridos de la prueba
+    public void passCredits (MasterStudies masterStudies, Player player) {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(101);
+        int pass = 0, deny = 0;
+        //Comprobar cuantos creditos pasa
+        for (int i = 0; i <= masterStudies.getNumberCredits() ; i++) {
+            if (randomNumber <= 100) {
+                //Credito conseguido
+                pass++;
+            }
+            else {
+                //Credito rechazado
+                deny++;
+            }
+        }
+        if (pass > deny) {
+            if (player.getPlayerType() == PlayerTypeOptions.ENGINEER) {
+                player.changePlayerType(PlayerTypeOptions.MASTERS);
+            } else {
+                player.incrementPI(3);
+                //checkStatus --> Futuro metodo que nos dira si el jugador ya puede evolucionar
+            }
+        } else {
+            player.decrementPI(3);
+        }
+    }
+}
